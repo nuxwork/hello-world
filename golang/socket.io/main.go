73e2af1d232b5c99a973f52b6648882de22d082a
mustodo <mustodo@aol.com>
2017-12-25 19:14:07 +0800

    socket.io for go

---
+++ golang/socket.io/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/googollee/go-socket.io"
+)
+
+func main() {
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server.On("connection", func(so socketio.Socket) {
+		log.Println("on connection")
+		so.Join("chat")
+		so.On("request", func(msg string) string {
+			e := so.Emit("response", msg)
+			if e != nil {
+				fmt.Println(e)
+			}
+			return "request end"
+		})
+
+		so.On("disconnection", func() {
+			log.Println("on disconnect")
+		})
+	})
+
+	server.On("error", func(so socketio.Socket, err error) {
+		log.Println("error:", err)
+	})
+
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("./asset")))
+	log.Println("Serving at localhost:5000...")
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
